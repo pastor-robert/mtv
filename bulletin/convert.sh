83b4aa446209c1cc2ac837b7419207834faf875b
Rob Adams <rob@methodist.cc>
2024-12-31 19:49:11 -0600

    final

--- bulletin/convert.sh
+++ bulletin/convert.sh
@@ -3,6 +3,8 @@
 for f in *.pdf
 do
 	g="${f%.*}.png"
-	echo "$f" "$g"
-	test -f "$g" || convert -colorspace rgb -strip "$f"[0] "$g"
+	test -f "$g" || {
+		echo "$f" "$g"
+		convert -colorspace rgb -strip "$f"[0] "$g"
+	}
 done
